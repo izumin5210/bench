6ae74ab6349fea99faae6aca71cbd13ca89cd3b1
izumin5210 <m@izum.in>
2017-12-20 18:19:36 +0900

    rid yarn add {,@types/}react-router-{dom,redux}

---
+++ frontend/src/app/configureStore.ts
@@ -0,0 +1,31 @@
+import { combineReducers, createStore, applyMiddleware } from "redux"
+import { combineEpics, createEpicMiddleware } from "redux-observable"
+
+// reducers and epics
+import { createAuthReducer, createAuthEpic } from "./auth/modules"
+
+// dependencies
+import AuthRepository from "../domain/AuthRepository"
+
+function createRedcuer() {
+  return combineReducers({
+    auth: createAuthReducer(),
+  })
+}
+
+function createEpic(authRepo: AuthRepository) {
+  return combineEpics(
+     createAuthEpic(authRepo),
+  )
+}
+
+export default function configureStore(authRepo: AuthRepository) {
+  const store = createStore(
+    createRedcuer(),
+    applyMiddleware(
+      createEpicMiddleware(createEpic(authRepo)),
+    ),
+  )
+
+  return store
+}
