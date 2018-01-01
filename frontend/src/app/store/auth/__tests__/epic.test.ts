70fbd04e9ea51fa9b036d28c1d685b4e2d1b4220
izumin5210 <m@izum.in>
2018-01-01 12:30:57 +0900

    Refactor epic test

--- frontend/src/app/store/auth/__tests__/epic.test.ts
+++ frontend/src/app/store/auth/__tests__/epic.test.ts
@@ -8,40 +8,44 @@ import AuthRepository from "domain/AuthRepository";
 import { actions, createAuthEpic } from "../"
 import { Action, Success } from "typescript-fsa";
 
-const createStoreAndEpic = (dependencies: { authRepository: AuthRepository }) => {
+const createTestContext = (authRepository: AuthRepository) => {
+  const dependencies = { authRepository }
   const epic = createAuthEpic()
   const mockStore = configureMockStore<any>([createEpicMiddleware(epic, { dependencies })])
   const store = mockStore()
-  return { store, epic }
+  const dispatch =
+    (a: Action<any>) => epic(ActionsObservable.of(a), store, dependencies).toArray().toPromise()
+  return { epic, dependencies, store, dispatch }
 }
 
-describe("authEpic", () => {
-  it("handle fetchAccessToken.stared", async () => {
-    const MockAuthRepository = jest.fn<AuthRepository>(() => ({
-        fetchAccessToken: jest.fn().mockReturnValueOnce(Promise.resolve({ token: "foobarbaz" })),
-    }))
-    const repo = new MockAuthRepository()
-    const dependencies = { authRepository: repo }
-    const { store, epic } = createStoreAndEpic(dependencies)
+describe("fetchAccessToken.started", () => {
+  describe("when an oauth state exists", () => {
+    it("maps a doen action with retrieved an access token", async () => {
+      const MockAuthRepository = jest.fn<AuthRepository>(() => ({
+          fetchAccessToken: jest.fn().mockReturnValueOnce(Promise.resolve({ token: "foo" })),
+      }))
+      const repo = new MockAuthRepository()
+      const { dispatch } = createTestContext(repo)
 
-    const action = actions.fetchAccessToken({ state: "foo", code: "bar" })
-    const gotActions = await epic(ActionsObservable.of(action), store, dependencies).toArray().toPromise()
+      const action = actions.fetchAccessToken({ state: "bar", code: "baz" })
+      const gotActions = await dispatch(action)
 
-    expect(repo.fetchAccessToken).toBeCalledWith({ state: "foo", code: "bar" })
-    expect(gotActions).toHaveLength(1)
-    expect((gotActions[0] as Action<Success<any, any>>).payload.result).toEqual({ token: "foobarbaz" })
+      expect(repo.fetchAccessToken).toBeCalledWith({ state: "bar", code: "baz" })
+      expect(gotActions).toHaveLength(1)
+      expect((gotActions[0] as Action<Success<any, any>>).payload.result).toEqual({ token: "foo" })
+    })
   })
+})
 
-  it("handle createOauthState.started", async () => {
+describe("createOauthState.started", () => {
+  it("maps a done action with created an oauth token", async () => {
     const MockAuthRepository = jest.fn<AuthRepository>(() => ({
         setOauthState: jest.fn().mockReturnValue(Promise.resolve(null)),
     }))
     const repo = new MockAuthRepository()
-    const dependencies = { authRepository: repo }
-    const { store, epic } = createStoreAndEpic(dependencies)
+    const { dispatch } = createTestContext(repo)
 
-    const action = actions.createOauthState({})
-    const gotActions = await epic(ActionsObservable.of(action), store, dependencies).toArray().toPromise()
+    const gotActions = await dispatch(actions.createOauthState({}))
 
     expect(repo.setOauthState).toBeCalledWith(expect.any(String))
     expect(gotActions).toHaveLength(1)
